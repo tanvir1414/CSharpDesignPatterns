fcd1a2e09a27f292e99bcfc1e09d21c4627c0998
Tanvir <Tanvir@LaptopAs>
2021-03-15 01:34:11 +0600

    Factory

---
+++ CSharpDesignPatterns/com.13factory/Subtract.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpDesignPatterns.com._13factory
+{
+    public class Subtract : ICalculate
+    {
+        public void Calculate(double a, double b)
+        {
+            Console.WriteLine("a - b is {0}", a - b);
+        }
+    }
+}
---
+++ CSharpDesignPatterns/com.13factory/Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpDesignPatterns.com._13factory
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            string input = Console.ReadLine();
+            double num1, num2;
+            bool result = Double.TryParse(input, out num1);
+            if (!result)
+            {
+                Console.WriteLine("Please enter a number");
+                return;
+            }
+            Console.WriteLine("Enter second number");
+            result = Double.TryParse(Console.ReadLine(), out num2);
+            if (!result)
+            {
+                Console.WriteLine("Please enter a number");
+                return;
+            }
+            Console.WriteLine("Enter Add, Substract or Divide");
+            CalculateFactory factory = new CalculateFactory();
+            ICalculate obj = factory.GetCalculation(Console.ReadLine());
+         //   Divide obj = new Divide();
+            obj.Calculate(num1, num2);
+        }
+    }
+}
